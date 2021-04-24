fde8b49c7a28f594d648f8edd64d900c4448137c
Justyna Budzioch <justyna.budzioch@gmail.com>
2021-04-24 15:56:21 +0200

    modified Drink class, added Main class

--- src/main/java/pl/sdacademy/drink/Drink.java
+++ src/main/java/pl/sdacademy/drink/Drink.java
@@ -1,4 +1,16 @@
 package pl.sdacademy.drink;
 
 public class Drink {
+    private String name;
+    private int volume;
+    private boolean fizzy;
+
+    public Drink(String name, int volume, boolean fizzy) {
+        this.name = name;
+        this.volume = volume;
+        this.fizzy = fizzy;
+    }
+    public String toString() {
+        return "Napój " + name + ", objętość " + volume + ", czy gazowany: " + fizzy;
+    }
 }
---
+++ src/main/java/pl/sdacademy/drink/Main.java
@@ -0,0 +1,8 @@
+package pl.sdacademy.drink;
+
+public class Main {
+    public static void main(String[] args) {
+        Drink water = new Drink("Cisowianka", 1500, true);
+        System.out.println(water);
+    }
+}
