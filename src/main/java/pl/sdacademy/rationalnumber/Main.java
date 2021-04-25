b18b4dbe1d95d9677a1eb3de2655a2204b6e0f02
Justyna Budzioch <justyna.budzioch@gmail.com>
2021-04-25 12:56:41 +0200

    RationalNumber Exercise 5-6

--- src/main/java/pl/sdacademy/rationalnumber/Main.java
+++ src/main/java/pl/sdacademy/rationalnumber/Main.java
@@ -7,8 +7,14 @@ public class Main {
         System.out.println("LIczba przeciwna do " + threeQuarters + " to : " + threeQuarters.getOpposite());
         System.out.println("LIczba odwrotna do " + threeQuarters + " to : " + threeQuarters.getInverse());
         System.out.println("zadanie 3");
-        System.out.println(threeQuarters.multiply(new RationalNumber(2,4)));
+        System.out.println(threeQuarters.multiply(new RationalNumber(2, 4)));
         System.out.println("zadanie 4");
-        System.out.println(threeQuarters.divide(new RationalNumber(2,3)));
+        System.out.println(threeQuarters.divide(new RationalNumber(2, 3)));
+        System.out.println("Zadanie 5 : ");
+        RationalNumber x = new RationalNumber(5);
+        System.out.println(x);
+        System.out.println("zadanie 6: ");
+        System.out.println(threeQuarters.add(8));
+
     }
 }
--- src/main/java/pl/sdacademy/rationalnumber/RationalNumber.java
+++ src/main/java/pl/sdacademy/rationalnumber/RationalNumber.java
@@ -9,32 +9,50 @@ public class RationalNumber {
         this.denominator = denominator;
     }
 
+    //5. Napisz konstruktor, który przyjmie jeden parametr - liczbę całkowitą.
+// Metoda zwróci liczbę wymierną równą zadanej liczbie całkowitej.
+    public RationalNumber(int x) {
+        this.numerator = x;
+        this.denominator = 1;
+    }
+
     public String toString() {
         return numerator + " / " + denominator;
     }
 
     public RationalNumber getOpposite() {
-        return  new RationalNumber(-numerator, denominator);
+        return new RationalNumber(-numerator, denominator);
     }
-//Napisz metode getInverse, która zwróci liczbę odwrotną (typu RationalNumber).
-     public RationalNumber getInverse() {
-        return new RationalNumber(denominator, numerator);
-     }
 
-     //3. Napisz metodę multiply, która przyjmie za parametr obiekt będący liczbą wymierną,
+    //Napisz metode getInverse, która zwróci liczbę odwrotną (typu RationalNumber).
+    public RationalNumber getInverse() {
+        return new RationalNumber(denominator, numerator);
+    }
+
+    //3. Napisz metodę multiply, która przyjmie za parametr obiekt będący liczbą wymierną,
     // a która zwróci wynik mnożenia obiektu, dla którego wywołano metodę przez parametr.
     //Dzięki metodzie multiply możemy pomnożyć dwie liczby wymierne - pierwszą będzie ta,
     // której metodę wywołamy, drugą będzie liczba przekazana przy użyciu parametru.
     // Metoda zwróci iloczyn tych dwóch liczb (wynikiem będzie oczywiście również liczba wymierna)
-    public RationalNumber multiply(RationalNumber number){
+    public RationalNumber multiply(RationalNumber number) {
         return new RationalNumber(this.numerator * number.numerator, this.denominator * number.denominator);
 
     }
-//    Napisz metodę divide, która zadziała analogicznie do metody multiply.
-    public RationalNumber divide(RationalNumber number2) {
-        return new RationalNumber(this.numerator * number2.numerator, this.denominator * number2.denominator);
+
+    // 4. Napisz metodę divide, która zadziała analogicznie do metody multiply.
+    public RationalNumber divide(RationalNumber divider) {
+//        return multiply(divider.getInverse()); dodatkowe rozwiązanie. getInvers odwrotnosc argumentu
+        return new RationalNumber(this.numerator * divider.denominator, this.denominator * divider.numerator);
     }
 
+    //6. Napisz w klasie RationalNumber metodę add,
+    // która za parametr przyjmie liczbę całkowitą.
+    // Metoda zwróci wartość liczby, dla której
+    // wywołano metodę powiększoną o wartość parametru
+    public RationalNumber add(int i) {
+        int resultNumerator = numerator + i * denominator;
+        return new RationalNumber(resultNumerator,denominator);
+    }
 
 
 }
